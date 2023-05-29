8c3394e91de3be9857f6a009c8113b67504e8390
haamid10 <apdixamiid919@gmail.com>
2023-05-29 17:06:21 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css'
-
-import Form from './components/Form'
-import Login from './components/Login'
-import User from './components/User'
-
-function App(){
-  return (
-    <div className='App'>
-      <Login/>
-
-      <Form/>
-
+import Header from "./Components/Header";
+import Search from "./Components/Search";
+import MovieCard from "./Components/MovieCard";
+function App() {
+ return (
+  <div classNameName="App">
+   <Header />
+   <div className="main">
+    <Search />
+    <div className="movies-section">
+     {/* <!-- one card --> */}
+     <MovieCard />
+     {/* <!-- one card --> */}
     </div>
-  )
+   </div>
+  </div>
+ );
 }
 
-
-export default App
-
+export default App;
