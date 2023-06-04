c54b063e719a15088ab684e9b3911abd410c66f0
haamid10 <apdixamiid919@gmail.com>
2023-06-04 17:45:30 +0300

    complete

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,18 +3,15 @@ import "../index.css";
 function MovieCard({movie}) {
   // const [ movie, setMovie] = useState(search)
 
-  console.log()
+  console.log(movie.image)
  return (
   <div className="movie-card">
    <div className="movie-image">
    <h3 className=" text-white text-3xl">{movie.name} </h3>
 
-     {/* <img
-     src={movie?.show.image.medium}
-     alt=""
-    />  */}
-    <h1>{movie.name}</h1>
-    {/* <img src={movie.image.medium} alt={movie.name} /> */}
+
+    {/* <h1>{movie.name}</h1> */}
+    <img src={movie.image.medium} alt='' />
     <h2>{movie.language}</h2>
    </div>
     
