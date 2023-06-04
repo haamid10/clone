1f8621a373e78896064e2856ca3c393b9e8a258e
haamid10 <apdixamiid919@gmail.com>
2023-06-04 17:25:46 +0300

    add

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,15 +1,13 @@
-import React,{useState} from 'react'
 
 const Search = ({handle, fetching}) => {
-  // const [search,setSearch] = useState([])
 
   
 
   // console.log(search)
   return (
     <div className=' flex items-center justify-center gap-12 '>
-      <input className=" h-12 text-black rounded-xl px-12 " type="text" placeholder="Search" onChange={ handle}   />
-      <button className=" bg-blue-600 p-2 w-24 rounded-xl text-white"  onClick={ fetching}  >Search</button>
+      <input className=" h-12 text-black rounded-xl px-12 " type="text" placeholder="Search" onChange={handle}   />
+      <button className=" bg-blue-600 p-2 w-24 rounded-xl text-white"  onClick={fetching}  >Search</button>
     </div>
   )
 }
