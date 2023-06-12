3a784ab5c3f134c5c003dbe19fe669f4e820ec8c
haamid10 <apdixamiid919@gmail.com>
2023-06-12 10:58:55 +0300

    solved

--- server/App.js
+++ server/App.js
@@ -1,31 +1,63 @@
-const express = require('express')
-
+const express = require('express');
 const app = express()
+
+app.use(express.json())
+
+const todos = require('./todos');
+
 
 // app.use((req,res,next(), => {
 //   console.log('middleware')
 //   next()
 // })
 
-app.use(express.json())
 
-app.get('/' ,(req,res)=> {
-  res.status(200).json({messege: 'added'})
+app.get('/todos' ,(req,res)=> {
+
+  res.status(200).json({messege: todos})
+
 });
 
-app.post('/' ,(req,res)=> {
-  res.status(200).json({data: req.body,messege:"added "})
+app.get('/todos/:id' ,(req,res)=> {
+
+  const result = todos.find((item)=> item.id == req.params.id)
+
+  res.status(200).json({messege: result})
+
 });
 
-app.put('/' ,(req,res)=> {
-  res.status(200).json({messege: 'update'})
+app.post('/todos' ,(req,res)=> {
+  const id = todos[todos.length=== 0] ? 1 : todos[todos.length-1].id+1;
+  const newTodo = {
+    id: id,
+    task: req.body.task ,
+    completed: req.body.completed
+  }
+  todos.push(newTodo)
+  res.status(200).json({data: todos,messege:"added "})
 });
 
-app.delete('/' ,(req,res)=> {
-  res.status(200).json({messege: 'delete'})
+app.put('/todos/:id' ,(req,res)=> {
+  const updated = todos.findIndex((item)=> item.id == req.params.id)
+  if(updated === -1){
+    res.status(404).json({messege: 'todos not found'})
+  }
+  const editTodo=(todos[updated].task = req.body.task)
+  res.status(200).json({messege: 'updated succesfuly'})
 });
 
-const port = 8000
+
+app.delete('/todos/:id' ,(req,res)=> {
+
+  const deletev= todos.findIndex((item)=> item.id == req.params.id)
+  if(deletev === -1){
+    res.status(404).json({messege: 'todos not found'})
+  }
+const result = todos.splice(deletev,1)
+  res.status(200).json({messege:  'deleted'})
+});
+
+const port = 8080
 app.listen(port , ()=> {
   console.log(`server is running on port ${port}`)
 })
---
+++ server/todos.js
@@ -0,0 +1,8 @@
+const todos = [
+  { id:0, task:"siuu" , completed:true},
+  { id:1, task:"alien" , completed:false},
+  { id:2, task:"human" , completed:true},
+  { id:3, task:"working" , completed:true}
+]
+
+module.exports = todos
