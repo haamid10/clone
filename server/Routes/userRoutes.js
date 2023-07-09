71dcee8b38c5c2450cec165e255e1fe5e1b6d9ed
haamid10 <apdixamiid919@gmail.com>
2023-07-09 21:26:47 +0300

    dd

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -4,6 +4,6 @@ const userController = require('../controlers/userControlers')
 
 
 router.post('/signup', userController.signup)
-
+router.get('/',userController.getAll)
 
 module.exports = router;
--- server/controlers/userControlers.js
+++ server/controlers/userControlers.js
@@ -1,26 +1,33 @@
 const data = require('../model/userModel')
+const bcrypt = require("bcrypt")
 
 exports.signup =async(req, res)=>{
-    const {name, email, password} = req.body
+    const {name, email, password, confirmPassword} = req.body
     try{
-      const user = await data.findOne({email: data.email});
-      if(user){
-        return  res.send("user already exist")
-        
+      const findUser = await data.findOne({ email});
+      if(findUser){
+          res.status(400).json("user already exist") 
+      }
+      if(password !== confirmPassword){
+        return res.status(400).send("passwords dont match")
       }
       
-
-      await data.create(req.body)
+      const hashedPassword =  bcrypt.hash(req.body.password, 10)
+      req.body.password = hashedPassword;
+      await data.create({...req.body})
       res.status(200).json('created successfully')
       }
     catch (error){
-        res.status(400).json('error')
+       return  res.status(400).json(error.message)
       }
-
-    
-    
-    
-
-
 }
 
+exports.getAll = async(req,res) => {
+  try{
+    const user = await data.find()
+    res.status(200).json(user)
+  }
+  catch(error){
+    res.status(400).json('error')
+  }   
+}
